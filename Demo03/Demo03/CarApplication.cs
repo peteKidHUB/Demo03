a4091a64a6a31f344c4025f8c6de363a36144986
peteKidHUB <pete95.hakala@gmail.com>
2017-01-26 10:47:37 +0200

    Demo03

---
+++ Demo03/Demo03/CarApplication.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo03
+{
+    class CarApplication
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public double Engine { get; set; }
+        public int Speed { get; set; }
+        public bool Dizes { get; set; }
+        public int DoorCount { get; set; }
+
+
+        //public CarApplication (string model);
+
+   //     {
+
+       //     Model = model;
+
+     //   }
+
+
+
+        public void Accelerate()
+        {
+
+            Speed += 10;
+
+        }
+
+        public void Break(int value)
+        {
+
+            Speed -= value;
+
+        }
+
+        public void PrintData()
+        {
+
+            Console.WriteLine("Car data: ");
+            Console.WriteLine("- model:" + Model);
+            Console.WriteLine("- color:" + Color);
+            Console.WriteLine("- engine:" + Engine);
+            Console.WriteLine("- speed" + Speed);
+            Console.WriteLine("- dizes:" + Dizes);
+            Console.WriteLine("- Doors:" + DoorCount);
+
+        }
+
+
+       // ~Car()
+
+        //{
+
+           // Console.WriteLine("Car Destoyed!!");
+
+        //}
+
+    }
+}
---
+++ Demo03/Demo03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CarApplication car = new CarApplication();
+            car.PrintData();
+            car.Model = "Datsun 100A";
+            car.Color = "Yellow";
+            car.Engine = 1.0;
+            car.Speed = 100;
+            car.Dizes = true;
+            car.DoorCount = 2;
+            car.PrintData();
+            car.Accelerate();
+            Console.WriteLine("Speed = " + car.Speed);
+
+
+            //CarApplication nascar = new CarApplication("Speedtrep");
+            //nascar.PrintData();
+
+        }
+    }
+}
